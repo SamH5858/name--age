543a4e0f2610f8294d3af04eb337367924647909
BuzzCo17 <81168131+BuzzCo17@users.noreply.github.com>
2021-05-05 09:39:43 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+basic.show_string("My name is: ")
+basic.show_string("Sam")
+basic.show_string("My age is: ")
+basic.show_number(14)
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showString("My name is: ")
+basic.showString("Sam")
+basic.showString("My age is: ")
+basic.showNumber(14)
